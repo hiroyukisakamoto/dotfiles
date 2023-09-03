d81e0276fc272aaa52ebe5cfc0f7da8b8313ab9d
jetz <jetz@jetz.local>
2023-09-03 14:29:11 +0900

    add install.sh

--- install.sh
+++ install.sh
@@ -36,7 +36,7 @@ if ! command -v brew > /dev/null 2>&1; then
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
     echo
 fi
-brew bundle
+brew bundle --global
 echo
 
 cat << END
